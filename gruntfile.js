93199d78fe184f251d912e5769c94a9a41968fc1
Sameet Nasnodkar <sameet.nasnodkar@gmail.com>
2016-02-05 21:49:11 -0500

    Include travis.yml for travis-ci integration

--- gruntfile.js
+++ gruntfile.js
@@ -1,15 +1,3 @@
-var _ = require('lodash');
-var packageJson = require('./package.json');
-
-var setupNpmInstall = function setupNpmInstall(grunt) {
-	grunt.config('exec', {
-		npm_install: 'npm install'
-	});
-	
-	grunt.registerTask('npm-install', 'exec:npm_install');
-};
-
 module.exports = function(grunt) {
-//	setupNpmInstall(grunt);
-	grunt.registerTask('build', ['clean']);
+	grunt.registerTask('build', []);
 };
