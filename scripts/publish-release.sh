3882215b953c6b1756ffe4afbc7dad4898b24772
Sameet Nasnodkar <sameet.nasnodkar@gmail.com>
2016-10-07 05:49:07 -0400

    Fixed release scripts

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -3,14 +3,13 @@
 export REPO_TAG=$CURR_VER-$TRAVIS_BUILD_NUMBER
 export GITHUB_USER=DevOpsPlayground
 export GITHUB_REPO=directory-react-nodejs
-export GITHUB_TOKEN=2e4f70bb81280660e7438fb479fa3bec8d7997c0
 git tag $REPO_TAG $TRAVIS_COMMIT -m "Tagging $GITHUB_REPO at $REPO_TAG"
 git push --tags
 
 # Package for deployment
 rm -rf node_modules
 npm install --no-optional --production
-tar czf /tmp/react-eg-$REPO_TAG.tar.gz . --exclude-from=./scripts/tar-excludes
+tar czvf /tmp/react-eg-$REPO_TAG.tar.gz . --exclude-from=./scripts/tar-excludes
 
 # download the github release binary
 wget https://github.com/aktau/github-release/releases/download/v0.6.2/linux-amd64-github-release.tar.bz2
